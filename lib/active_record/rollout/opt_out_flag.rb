846695aae5247021162dcbaafae2d9051aaf8d77
Jonathan Clem <jonathan@jclem.net>
2013-12-16 20:58:55 -0800

    Validate feature uniqueness for OptOutFlag

--- lib/active_record/rollout/opt_out_flag.rb
+++ lib/active_record/rollout/opt_out_flag.rb
@@ -4,6 +4,7 @@ class ActiveRecord::Rollout::OptOutFlag < ActiveRecord::Rollout::Flag
   belongs_to :flaggable, polymorphic: true
 
   validates :flaggable_id, presence: true
+  validates :feature_id, uniqueness: { scope: [:flaggable_type, :flaggable_id] }
 
   attr_accessible :flaggable
 end
