bda3096eaad2e700051c6c68ab9ba06135ef7001
Jonathan Clem <jonathan@jclem.net>
2013-12-18 13:10:26 -0800

    Spec for listing grepped features

---
+++ spec/features/features_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+describe "listing features for a type" do
+  let!(:feature) { create :feature }
+
+  before do
+    Detour.config.grep_dirs = %w[spec/dummy/app/**/*.{rb,erb}]
+    visit "/detour/flags/user"
+  end
+
+  it "lists every persisted feature" do
+    page.should have_content feature.name
+  end
+
+  it "lists features found in the codebase" do
+    page.should have_content "show_widget_table"
+  end
+end
