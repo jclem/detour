92109e404f0c96c8bcbecbb6a49825325c6bc5af
Jonathan Clem <jonathan@jclem.net>
2013-12-20 15:42:08 -0800

    Validate feature, not feature_id on flag

--- spec/models/detour/flag_spec.rb
+++ spec/models/detour/flag_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe Detour::Flag do
   it { should belong_to :feature }
-  it { should validate_presence_of :feature_id }
+  it { should validate_presence_of :feature }
   it { should validate_presence_of :flaggable_type }
   it { should allow_mass_assignment_of :flaggable_type }
 end
