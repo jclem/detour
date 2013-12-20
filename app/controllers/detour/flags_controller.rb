64f4d3326d71093d81c9109f95721c0f24271b28
Jonathan Clem <jonathan@jclem.net>
2013-12-19 22:00:31 -0800

    Creating new percentage flags

--- app/controllers/detour/flags_controller.rb
+++ app/controllers/detour/flags_controller.rb
@@ -2,7 +2,22 @@ class Detour::FlagsController < Detour::ApplicationController
   before_filter :ensure_flaggable_type_exists
 
   def index
-    @features = Detour::Feature.with_lines
+    @features = Detour::Feature.includes("#{params[:flaggable_type]}_percentage_flag").with_lines
+  end
+
+  def update
+    @features = Detour::Feature.includes("#{params[:flaggable_type]}_percentage_flag").with_lines
+
+    results = @features.map do |feature|
+      feature.update_attributes params[:features][feature.name]
+    end
+
+    if results.all?
+      flash[:notice] = "Your flags have been successfully updated."
+      redirect_to flags_path
+    else
+      render :index
+    end
   end
 
   private
--- spec/controllers/detour/flags_controller_spec.rb
+++ spec/controllers/detour/flags_controller_spec.rb
@@ -16,11 +16,62 @@ describe Detour::FlagsController do
     it "renders the 'index' template" do
       response.should render_template "index"
     end
+
+    context "when the type is not defined" do
+      it "raisese a 404" do
+        expect { get :index, flaggable_type: "user" }.to raise_error ActionController::RoutingError
+      end
+    end
   end
 
-  context "when the type is not defined" do
-    it "raisese a 404" do
-      expect { get :index, flaggable_type: "user" }.to raise_error ActionController::RoutingError
+  describe "POST #update" do
+    it "assigns every feature with lines" do
+      post :update, flaggable_type: "users"
+      assigns(:features).collect(&:name).should eq Detour::Feature.with_lines.collect(&:name)
+    end
+
+    context "when the save is successful" do
+      let!(:feature) { create :feature, name: "foo_feature" }
+
+      before do
+        post :update, flaggable_type: "users", features: {
+          foo_feature: {
+            users_percentage_flag_attributes: { percentage: 50 }
+          }
+        }
+      end
+
+      it "saves the flags" do
+        feature.users_percentage_flag.percentage.should eq 50
+      end
+
+      it "redirects to the flags path" do
+        response.should redirect_to flags_path
+      end
+
+      it "sets a flags message" do
+        flash[:notice].should eq "Your flags have been successfully updated."
+      end
+    end
+
+    context "when the save is unsuccessful" do
+      let!(:feature) { create :feature, name: "foo_feature" }
+
+      before do
+        post :update, flaggable_type: "users", features: {
+          foo_feature: {
+            users_percentage_flag_attributes: { percentage: "foo" }
+          }
+        }
+      end
+
+      it "does not save anything" do
+        feature.users_percentage_flag.should_not be_persisted
+      end
+
+      it "renders the index template" do
+        response.should render_template "index"
+      end
     end
   end
 end
