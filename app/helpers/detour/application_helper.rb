60c98806b95e3afed00563f397330b8e56a2fbeb
Jonathan Clem <jonathan@jclem.net>
2013-12-21 09:17:51 -0800

    Modal helpers

--- app/helpers/detour/application_helper.rb
+++ app/helpers/detour/application_helper.rb
@@ -6,4 +6,27 @@ module Detour::ApplicationHelper
       end
     end
   end
+
+  def modal(options, &block)
+    content_tag :div, id: options[:id], class: "modal #{options[:fade].present?}", tabindex: "-1", role: "dialog", aria_labbeledby: "#{options[:id]}-modal-label", aria_hidden: "true" do
+      content_tag :div, class: "modal-dialog" do
+        content_tag :div, class: "modal-content" do
+          content_tag(:div, class: "modal-header") do
+            content_tag :button, "&times;", class: "close", data_dismiss: "modal", aria_hidden: "true"
+            content_tag :h4, options[:title], id: "#{options[:id]}-modal-label"
+          end +
+
+          content_tag(:div, class: "modal-body") do
+            yield
+          end
+        end
+      end
+    end
+  end
+
+  def modal_footer(&block)
+    content_tag :div, class: "modal-footer" do
+      yield
+    end
+  end
 end
