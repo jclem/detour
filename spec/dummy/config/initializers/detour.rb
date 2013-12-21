28f7548cbd47b27a71257e195375cf4c99e77541
Jonathan Clem <jonathan@jclem.net>
2013-12-20 22:21:00 -0800

    Table style update

--- spec/dummy/config/initializers/detour.rb
+++ spec/dummy/config/initializers/detour.rb
@@ -7,4 +7,28 @@ Detour.configure do |config|
 
   config.define_users_group :admin do |admin|
   end
+
+  config.define_users_group :admin1 do |admin|
+  end
+
+  config.define_users_group :admin2 do |admin|
+  end
+
+  config.define_users_group :admin3 do |admin|
+  end
+
+  config.define_users_group :admin4 do |admin|
+  end
+
+  config.define_users_group :admin5 do |admin|
+  end
+
+  config.define_users_group :admin6 do |admin|
+  end
+
+  config.define_users_group :long_group_name_yup do |admin|
+  end
+
+  config.define_users_group "Multi-word Group Name" do |admin|
+  end
 end
