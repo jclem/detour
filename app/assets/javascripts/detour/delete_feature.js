bf013e5f537e54a05e9eb34bacdd96100fc49102
Jonathan Clem <jonathan@jclem.net>
2014-01-25 15:38:29 -0800

    Remove unnecessary jQuery wrapper functions

--- app/assets/javascripts/detour/delete_feature.js
+++ app/assets/javascripts/detour/delete_feature.js
@@ -1,13 +1,11 @@
-$(function () {
-  $('.delete-feature').on('click', function (e) {
-    var href    = $(e.currentTarget).data('path'),
-        feature = $(e.currentTarget).closest('td').next().text(),
-        $modal  = $('#delete-feature'),
-        $name   = $modal.find('.feature-name'),
-        $link   = $modal.find('a');
+$(document).on('click', '.delete-feature', function (e) {
+  var href    = $(e.currentTarget).data('path'),
+      feature = $(e.currentTarget).closest('td').next().text(),
+      $modal  = $('#delete-feature'),
+      $name   = $modal.find('.feature-name'),
+      $link   = $modal.find('a');
 
-    $link.attr('href', href);
-    $name.text(feature.trim());
-    $modal.modal('show');
-  });
+  $link.attr('href', href);
+  $name.text(feature.trim());
+  $modal.modal('show');
 });
